f2ba7c331f3b722eeb3cb217afe27774b45d25b4
ant6 <ant6ant@gmail.com>
2016-06-05 18:01:16 +0200

    Flow model

--- netstatus/dashboard/models.py
+++ netstatus/dashboard/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+class Flow(models.Model):
+    PROTOCOLS = (
+        (6, 'TCP'),
+        (17, 'UDP'),
+    )
+
+    RISKS = (
+        (0, 'low'),
+        (1, 'neutral'),
+        (2, 'moderate'),
+        (3, 'high'),
+    )
+
+    date = models.DateField()
+    source_ip = models.GenericIPAddressField()
+    protocol = models.SmallIntegerField(choices=PROTOCOLS)
+    source_port = models.IntegerField()
+    target_port = models.IntegerField()
+    risk = models.SmallIntegerField(choices=RISKS)
