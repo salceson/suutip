6e783c48608f195074c6c3677ebd5b84c4214748
Mateusz Małek <mamalek@student.agh.edu.pl>
2016-06-26 00:53:13 +0200

    Fix typo in docker-ovs driver

--- docker-ovs/docker_ovs/plugin/__init__.py
+++ docker-ovs/docker_ovs/plugin/__init__.py
@@ -85,7 +85,7 @@ def net_drv_join():
     hosts_ifs = db['endpoints']
     hosts_ifs[json_request['EndpointID']] = host_if
     db['endpoints'] = hosts_ifs
-    br_inet_addr = nf.ifaddresses(DOCKER_OVS_BRIDGE)[ni.AF_INET]
+    br_inet_addr = ni.ifaddresses(DOCKER_OVS_BRIDGE)[ni.AF_INET]
     gateway_ip = br_inet_addr[0]['addr']
     json_response = {
         'InterfaceName': {
