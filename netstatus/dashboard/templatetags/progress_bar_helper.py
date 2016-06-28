59531b62d721bb02b8bd0647047ecce131635714
Michał Ciołczyk <ciolczyk.michal.93@gmail.com>
2016-06-28 23:48:37 +0200

    Fix for progess bars

--- netstatus/dashboard/templatetags/progress_bar_helper.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.simple_tag
-def progress_bar_width(this_count, obj_count, max_value):
-    return "%.2f" % (float(this_count) / float(obj_count) * float(max_value))
