f78066a515c199fcd8d32a46633bd47441c79eb7
Mateusz Małek <mamalek@student.agh.edu.pl>
2016-06-28 02:44:19 +0200

    Add support for external database

--- microservices/users/database.py
+++ microservices/users/database.py
@@ -1,8 +1,11 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///users.db', convert_unicode=True)
+
+engine = create_engine(os.environ.get('DB_CONNECT_STRING', 'sqlite:///users.db'), convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
@@ -14,6 +17,5 @@ def init_db():
     Base.metadata.create_all(bind=engine)
 
 
-if __name__ == '__main__':
-    init_db()
+init_db()
 
