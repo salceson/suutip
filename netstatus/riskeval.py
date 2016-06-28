be47e2bc6c2c6eb637b88d7a7b988d0250c5eddb
Mateusz Małek <mamalek@student.agh.edu.pl>
2016-06-28 15:45:32 +0200

    Add initial version of risk evaluator

---
+++ netstatus/riskeval.py
@@ -0,0 +1,155 @@
+import os
+import sys
+from time import sleep
+from threading import Thread
+
+import django
+from django.db import transaction
+
+import docker
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "netstatus.settings")
+django.setup()
+
+from dashboard.models import Flow, Protocols, Risks
+
+client = docker.Client.from_env()
+
+containers = {}
+services = {
+    '10.10.10.1': 'gateway',
+    '10.10.10.254': 'lb',
+}
+
+def handle_start(container_id):
+    container = client.inspect_container(container_id)
+    if container['Config']['Labels'].get('com.docker.compose.project') != 'microservices':
+        return
+    service = container['Config']['Labels']['com.docker.compose.service']
+    suutip_network = container['NetworkSettings']['Networks'].get('suutip')
+    if not suutip_network:
+        return
+    ip_addr = suutip_network['IPAddress']
+    containers[container_id] = ip_addr
+    services[ip_addr] = service
+
+def handle_stop(container_id):
+    ip_addr = containers[container_id]
+    del containers[container_id]
+    del services[ip_addr]
+
+for container in client.containers():
+    container_id = container['Id']
+    handle_start(container_id)
+print(containers)
+print(services)
+print('---')
+
+def handle_docker_events():
+    for event in client.events(decode=True):
+        if event['Type'] != 'container':
+            continue
+        if event['Action'] not in ('start', 'stop'):
+            continue
+        container_id = event['Actor']['ID']
+        print(event['Type'], event['Action'], container_id)
+        if event['Action'] == 'start':
+            handle_start(container_id)
+        elif event['Action'] == 'stop':
+            handle_stop(container_id)
+        print(containers)
+        print(services)
+        print('---')
+
+t = Thread(target=handle_docker_events)
+t.daemon = True
+t.start()
+
+from random import randint
+
+def calculate_risk(flow):
+    src_ip = flow.source_ip
+    dst_ip = flow.target_ip
+    proto = flow.protocol
+    src_port = flow.source_port
+    dst_port = flow.target_port
+    src_srv = services.get(src_ip, 'external')
+    dst_srv = services.get(dst_ip, 'external')
+    if src_srv == 'gateway':
+        flow.risk = Risks.high.value
+    elif src_srv == 'lb':
+        if dst_srv in ('ipdiag', 'users', 'aggregate'):
+            if proto == Protocols.TCP.value and dst_port == 5000: flow.risk = Risks.low.value
+            elif proto == Protocols.ARP.value: flow.risk = Risks.low.value
+            else: flow.risk = Risks.high.value
+        elif dst_srv == 'gui':
+            if proto == Protocols.TCP.value and dst_port == 3001: flow.risk = Risks.low.value
+            elif proto == Protocols.ARP.value: flow.risk = Risks.low.value
+            else: flow.risk = Risks.high.value
+        elif dst_srv == 'external':
+            if proto == Protocols.TCP.value and src_port in (80, 443): flow.risk = Risks.moderate.value
+            else: flow.risk = Risks.high.value
+        else: flow.risk = Risks.high.value
+    elif src_srv == 'ipdiag':
+        if dst_srv in ('lb', 'aggregate'):
+            if proto == Protocols.TCP.value and src_port == 5000: flow.risk = Risks.low.value
+            elif proto == Protocols.ARP.value: flow.risk = Risks.low.value
+            else: flow.risk = Risks.high.value
+        elif dst_srv == 'external':
+            if proto == Protocols.ICMP.value: flow.risk = Risks.moderate.value
+            else: flow.risk = Risks.high.value
+        else: flow.risk = Risks.high.value
+    elif src_srv == 'users':
+        if dst_srv in ('lb', 'aggregate'):
+            if proto == Protocols.TCP.value and src_port == 5000: flow.risk = Risks.low.value
+            elif proto == Protocols.ARP.value: flow.risk = Risks.low.value
+            else: flow.risk = Risks.high.value
+        elif dst_srv == 'db':
+            if proto == Protocols.TCP.value and dst_port == 5432: flow.risk = Risks.low.value
+            elif proto == Protocols.ARP.value: flow.risk = Risks.low.value
+            else: flow.risk = Risks.high.value
+        else: flow.risk = Risks.high.value
+    elif src_srv == 'aggregate':
+        if dst_srv == 'lb':
+            if proto == Protocols.TCP.value and src_port == 5000: flow.risk = Risks.low.value
+            elif proto == Protocols.ARP.value: flow.risk = Risks.low.value
+            else: flow.risk = Risks.high.value
+        elif dst_srv in ('ipdiag', 'users'):
+            if proto == Protocols.TCP.value and dst_port == 5000: flow.risk = Risks.low.value
+            elif proto == Protocols.ARP.value: flow.risk = Risks.low.value
+            else: flow.risk = Risks.high.value
+        else: flow.risk = Risks.high.value
+    elif src_srv == 'gui':
+        if dst_srv == 'lb':
+            if proto == Protocols.TCP.value and src_port == 3001: flow.risk = Risks.low.value
+            elif proto == Protocols.ARP.value: flow.risk = Risks.low.value
+            else: flow.risk = Risks.high.value
+        else: flow.risk = Risks.high.value
+    elif src_srv == 'db':
+        if dst_srv == 'users':
+            if proto == Protocols.TCP.value and src_port == 5432: flow.risk = Risks.low.value
+            elif proto == Protocols.ARP.value: flow.risk = Risks.low.value
+            else: flow.risk = Risks.high.value
+        else: flow.risk = Risks.high.value
+    elif src_srv == 'external':
+        if dst_srv == 'gw': flow.risk = Risks.high.value
+        elif dst_srv == 'lb':
+            if proto == Protocols.TCP.value and dst_port in (80, 443): flow.risk = Risks.moderate.value
+            else: flow.risk = Risks.high.value
+        elif dst_srv == 'ipdiag':
+            if proto == Protocols.ICMP.value: flow.risk = Risks.moderate.value
+            else: flow.risk = Risks.high.value
+        else: flow.risk = Risks.high.value
+    else: flow.risk = Risks.high.value
+    flow.save()
+
+last_id = Risks.neutral.value
+
+Flow.objects.update()
+
+while True:
+    for flow in Flow.objects.filter(id__gt=last_id):
+        print(flow.id, flow.protocol, flow.source_ip, flow.target_ip)
+        calculate_risk(flow)
+        last_id = flow.id
+    sleep(5)
